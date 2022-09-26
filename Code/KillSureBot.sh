7044bcdf69f382a7c6320767b034a1c8ccd91cab
PL Yap <powlook@gmail.com>
2022-09-26 15:36:11 +0800

    remove old scripts

--- Code/KillSureBot.sh
+++
@@ -1,3 +0,0 @@
-# Script to kill BOT process
-
-pkill -f supervisord
--- Code/StartSureBot.sh
+++
@@ -1,6 +0,0 @@
-# Script to start up webhook for SureBot
-
-# Start Process Supervisor
-supervisord -c supervisord.conf
-
-python start_backend.py
